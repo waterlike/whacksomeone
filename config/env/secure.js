9bc800f091e66b190851c67adbd186aefa6bc1ed
Lee Zheng Tao <waterlike86@gmail.com>
2015-06-22 22:12:33 +0800

    initial commit for meanjs

---
+++ config/env/secure.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+	port: 443,
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/whacksomeone',
+	assets: {
+		lib: {
+			css: [
+				'public/lib/bootstrap/dist/css/bootstrap.min.css',
+				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+			],
+			js: [
+				'public/lib/angular/angular.min.js',
+				'public/lib/angular-resource/angular-resource.min.js',
+				'public/lib/angular-animate/angular-animate.min.js',
+				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+				'public/lib/angular-ui-utils/ui-utils.min.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+			]
+		},
+		css: 'public/dist/application.min.css',
+		js: 'public/dist/application.min.js'
+	},
+	facebook: {
+		clientID: process.env.FACEBOOK_ID || '1453660684934131',
+		clientSecret: process.env.FACEBOOK_SECRET || '90ae0cd102d3f4dc6a3bae6ee986c448',
+		callbackURL: 'https://localhost:443/auth/facebook/callback'
+	},
+	twitter: {
+		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
+		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+		callbackURL: 'https://localhost:443/auth/twitter/callback'
+	},
+	google: {
+		clientID: process.env.GOOGLE_ID || 'APP_ID',
+		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
+		callbackURL: 'https://localhost:443/auth/google/callback'
+	},
+	linkedin: {
+		clientID: process.env.LINKEDIN_ID || 'APP_ID',
+		clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+		callbackURL: 'https://localhost:443/auth/linkedin/callback'
+	},
+	github: {
+		clientID: process.env.GITHUB_ID || 'APP_ID',
+		clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
+		callbackURL: 'https://localhost:443/auth/github/callback'
+	},
+	mailer: {
+		from: process.env.MAILER_FROM || 'MAILER_FROM',
+		options: {
+			service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+			auth: {
+				user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
+				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+			}
+		}
+	}
+};
