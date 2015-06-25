75e3fd3bade2e09f59006c7b0fc807fdb08d826e
Lee Zheng Tao <waterlike86@gmail.com>
2015-06-25 23:29:00 +0800

    try integrating worm p1

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,6 +11,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		}).
+		state('worm', {
+			url: '/worm',
+			templateUrl: 'modules/core/views/worm.client.view.html'
 		});
 	}
 ]);
