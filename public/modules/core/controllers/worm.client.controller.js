67d5bf8a70c1e255f6666acc6e4427fb94677370
Lee Zheng Tao <waterlike86@gmail.com>
2015-07-06 22:17:55 +0800

    - prevent get started from opening another page

--- public/modules/core/controllers/worm.client.controller.js
+++ public/modules/core/controllers/worm.client.controller.js
@@ -5,5 +5,9 @@ angular.module('core').controller('WormController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		$scope.init = function(){
+			alert("init worm!");
+		};
 	}
 ]);
